81bed99c96e4ea0354190430adec04e40241c4b9
Jess Teixeira <61021800+jteixeir@users.noreply.github.com>
2020-05-06 16:58:28 -0300

    Add files via upload

---
+++ color.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteixeir <jteixeir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/04 15:01:51 by jteixeir          #+#    #+#             */
+/*   Updated: 2020/05/06 15:14:23 by jteixeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+#define RED             "\x1b[31m"
+#define GREEN           "\x1b[32m"
+#define YELLOW          "\x1b[33m"
+#define BLUE            "\x1b[34m"
+#define MAGENTA         "\x1b[35m"
+#define CYAN            "\x1b[36m"
+#define COLOR_RESET     "\x1b[0m"
+
